820e514db2724c57c06fb19e68a21dccb817f995
bhayden51 <bradley8451@hotmail.com>
2020-02-19 21:31:51 -0600

    Added Title scrrens code

---
+++ Assets/TitleTransitions.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleTransitions : MonoBehaviour
+{
+    public Animator anim;
+    public string sceneName;
+
+    public void FadeOut()
+    {
+        anim.SetTrigger("Fade Out");
+        StartCoroutine(WaitHalfSecond());
+    }
+
+    IEnumerator WaitHalfSecond()
+    {
+        yield return new WaitForSeconds(.5f);
+        SceneManager.LoadScene(sceneName);
+    }
+
+}
