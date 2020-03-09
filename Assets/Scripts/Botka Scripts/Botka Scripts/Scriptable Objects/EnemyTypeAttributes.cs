821bc38246618909a83543c229a6e359c3a0f812
Botka.Jake <jake.m.botka@gmail.com>
2020-03-08 23:01:17 -0500

    More Botka Scripts

---
+++ Assets/Scripts/Botka Scripts/Botka Scripts/Scriptable Objects/EnemyTypeAttributes.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "EnemyTypeData", menuName = "EnmyTypeData")]
+public class EnemyTypeAttributes : ScriptableObject
+{
+    public enum AttackType
+    {
+        Gun,Meele,None
+    }
+
+   [SerializeField] private AttackType attackType;
+   [SerializeField] private float damage;
+    [SerializeField]private float movementSpeedModifeier;
+
+
+    public AttackType getAttackType()
+    {
+        return attackType;
+    }
+    public void setAttackType(AttackType attackType)
+    {
+        this.attackType = attackType;
+    }
+
+   
+}
